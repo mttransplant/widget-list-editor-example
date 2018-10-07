1e09cf0d177d0cd98fc79768e6881ae679a2d93b
Adam Smith <apsmith@ccs.neu.edu>
2018-10-07 16:19:44 -0400

    small cleanup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Widget from "./components/Widget";
-import WidgetList from "./components/WidgetList";
 import App from "./App";
 
 ReactDOM.render(
-    // <Widget widget={{text: "Hello"}}/>,
-    // <WidgetList widgets={[{text: "heading"},{text: "list"}]}/>,
     <App/>,
     document.getElementById('root')
 )
